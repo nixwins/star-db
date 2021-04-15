ef8afdb8bd4a2087603927a437298c828df909fe
Zhoraev D <newlifenewgoal@gmail.com>
2021-04-15 16:32:45 +0600

    добавление методов lifecycle methods

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,39 +1,62 @@
 import React, { Component } from 'react';
+import Error from './../error';
 import SwapiService from '../../services/swapi-service';
+
 import './random-planet.css'
 
 export default class RandomPlanet extends Component{
 
+    _request = new SwapiService();
 
     state = {
-        id:null,
-        name:null,
-        population:null,
-        rotationPeriod:null,
-        diameter:null
+        planet:{},
+        loading:true,
+        error:false
+    };
+
+    componentDidMount(){
+        this.updatePlanet();
+        setInterval(this.updatePlanet, 5000);  
     }
 
-    constructor(){
-        super();
-        const request = new SwapiService();
+    updatePlanet = ()=>{
+
         const id = Math.floor(Math.random()*20) + 2;
-        request.getPlanet(id)
-               .then((planet)=>{
-                    this.setState({
-                        id,
-                        name:planet.name,
-                        population:planet.population,
-                        rotationPeriod:planet.rotation_period,
-                        diameter:planet.diameter
-                     });
-                });
+        // console.log(id)
+        this._request.getPlanet(id)
+        .then(this.onLoaded)
+        .catch(this.onError)
+    };
+
+    onLoaded = (planet)=>{
+        this.setState({planet, loading:false})
+    };
+
+    onError = ()=>{
+        this.setState({error:true})
     }
     render(){
 
-        const {id, name, population, rotationPeriod, diameter} =  this.state;
+        const {loading, error, planet} =  this.state;
+        const spin = loading && !error  ? <Spin/> : null;
+        const planetViewer = !loading && !error  ? <PlanetViewer planet={planet}/> : null;
+        const errorEl =  error ? <Error/> : null;
+
         return (
                 <div className="random-planet">
-                    <div className="rp-img-wrapper">
+                    {spin}
+                    {planetViewer}
+                    {errorEl}
+                </div>
+        )
+    }
+}
+const PlanetViewer = ({planet})=>{
+
+   const {id, name, population, rotationPeriod, diameter} = planet;
+
+    return (<React.Fragment >
+                <div className="rp-img-wrapper">
                         <img src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} alt={name} />
                     </div>
                     <div className="rp-description">
@@ -41,8 +64,15 @@ export default class RandomPlanet extends Component{
                         <p>Population: <span>{population}</span></p>
                         <p>Rotation period: <span>{rotationPeriod}</span></p>
                         <p>Diameter: <span>{diameter}</span></p>
-                    </div>
                 </div>
-        )
-    }
+            </React.Fragment>)
+}
+const Spin = ()=>{
+    return(
+        <div className="loadingio-spinner-interwind-nqrxppgvd4o">
+            <div className="ldio-7hink7wze5f">
+                 <div><div><div><div></div></div></div><div><div><div></div></div></div></div>
+            </div>
+        </div>
+    )
 }
